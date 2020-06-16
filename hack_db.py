7c5b0a3708d818e929ce0aa879c2ae747ae478dd
Kosmostars7403 <64686091+Kosmostars7403@users.noreply.github.com>
2020-06-16 13:17:10 +0300

    Update hack_db.py

--- hack_db.py
+++ hack_db.py
@@ -2,15 +2,19 @@ import random
 from datacenter.models import Schoolkid, Lesson, Subject, Mark, Commendation, Chastisement
 
 
-def fix_marks(name):
+def check_schoolkid_exists(name):
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=name)
     except Schoolkid.MultipleObjectsReturned:
         print('Найдено несколько учеников с таким именем. Пожалуйста, уточните запрос и попробуйте заново.')
-        exit()
+        exit(1)
     except Schoolkid.DoesNotExist:
         print('Указанный пользователь не существует. Отправьте запрос заново.')
-        exit()
+        exit(1)
+    return schoolkid
+
+def fix_marks(name):
+    schoolkid = check_schoolkid_exists(name)
     bad_marks = Mark.objects.filter(schoolkid__full_name=schoolkid.full_name, points__in=[2, 3])
     for bad_mark in bad_marks:
         good_mark = random.randint(4, 5)
@@ -20,27 +24,13 @@ def fix_marks(name):
 
 
 def remove_chastisements(name):
-    try:
-       schoolkid = Schoolkid.objects.get(full_name__contains=name)
-    except Schoolkid.MultipleObjectsReturned:
-        print('Найдено несколько учеников с таким именем. Пожалуйста, уточните запрос и попробуйте заново.')
-        exit()
-    except Schoolkid.DoesNotExist:
-        print('Указанный пользователь не существует. Отправьте запрос заново.')
-        exit()
+    schoolkid = check_schoolkid_exists(name)
     child_chastisements = Chastisement.objects.filter(schoolkid__full_name=schoolkid.full_name)
     return child_chastisements.delete()
 
 
 def create_commendation(name, subject):
-    try:
-        schoolkid = Schoolkid.objects.get(full_name__contains=name)
-    except Schoolkid.MultipleObjectsReturned:
-        print('Найдено несколько учеников с таким именем. Пожалуйста, уточните запрос и попробуйте заново.')
-        exit()
-    except Schoolkid.DoesNotExist:
-        print('Указанный пользователь не существует. Отправьте запрос заново.')
-        exit()
+    schoolkid = check_schoolkid_exists(name)
     praise = random.choice(praises)
     try:
         lesson = Lesson.objects.filter(group_letter='А', year_of_study=6, subject__title=subject).order_by('-date').first()
@@ -48,7 +38,7 @@ def create_commendation(name, subject):
                                                subject=lesson.subject, teacher=lesson.teacher)
     except AttributeError:
         print('Указанная дисциплина не найдена, попробуйте ввести еще раз!')
-        exit()
+        exit(1)
     return commendation
 
 
